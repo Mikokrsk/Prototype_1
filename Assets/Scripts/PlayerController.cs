964de6fac3fab8e81137d6c3170e9743f36fe8cb
Mykola Krasko <mikokrsk@gmail.com>
2023-07-05 13:46:45 +0300

    Added Speedometer
    +Import TMP text
    +Added Speedometer text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // public float speed = 12.0f;
-   [SerializeField] private float horsePower = 100f;
-   [SerializeField] private float turnSpeed = 5.0f;
+    [SerializeField] private float horsePower = 100f;
+    [SerializeField] private float turnSpeed = 5.0f;
+    [SerializeField] private float speed;
     private float horizontalInput;
     private float verticalInput;
     private string horizontalAxis;
     private string verticalAxis;
     private Rigidbody playerRb;
     [SerializeField] private GameObject centerOfMass;
+    [SerializeField] private TextMeshProUGUI speedometerText;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,14 +30,16 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f);
+        speedometerText.SetText("Speed : "+speed+" km/h");
     }
 
     private void SetAxes()
     {
         if (gameObject.CompareTag("Player_1"))
         {
-            horizontalAxis = "Horizontal1";
-            verticalAxis = "Vertical1";
+            horizontalAxis = "Horizontal";
+            verticalAxis = "Vertical";
         }
         else
         {
